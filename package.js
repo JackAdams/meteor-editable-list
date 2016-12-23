c19fad9a2186ff3045851078063dfc814f22c158
Brent Abrahams <brent_abrahams@yahoo.com>
2016-12-24 03:57:11 +1300

    Updated dependencies

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'babrahams:editable-list',
   summary: 'Editable list widget for Meteor',
-  version: '0.4.30',
+  version: '0.4.31',
   git: 'https://github.com/jackadams/meteor-editable-list.git'
 });
 
@@ -9,7 +9,7 @@ Package.onUse(function(api) {
     
   api.versionsFrom('1.0');
   
-  api.use('babrahams:editable-text@0.8.30', ['client','server']);
+  api.use('babrahams:editable-text@0.8.31', ['client','server']);
   api.imply('babrahams:editable-text');
   api.use('jquery', 'client');
   // api.use('mizzao:jquery-ui@1.11.2', 'client'); // Removed in 0.3.0 to give devs flexibility about which version of jqueryui to use, or to leave it out completely
@@ -22,8 +22,6 @@ Package.onUse(function(api) {
   api.use('underscore', ['client','server']);
   api.use('mongo', ['client','server']);
   api.use('reactive-var', 'client');
-  api.use('djedi:sanitize-html@1.11.3', 'server');
-  api.imply('djedi:sanitize-html');
   
   api.addFiles('lib/editable-list-common.js', ['client','server']);
   api.addFiles('lib/editable-list.css','client');
