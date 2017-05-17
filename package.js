86dd72886ce642a7ca00c12f1bdc51547782fc84
Brent Abrahams <brent_abrahams@yahoo.com>
2017-05-18 00:34:54 +0800

    Fixed potential edge case bug

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'babrahams:editable-list',
   summary: 'Editable list widget for Meteor',
-  version: '0.5.4',
+  version: '0.5.5',
   git: 'https://github.com/jackadams/meteor-editable-list.git'
 });
 
