2b98fe3eb2e3ea73d9bc78514ab21aa706d16e6d
Brent Abrahams <brent_abrahams@yahoo.com>
2020-01-05 21:56:52 +0800

    Fixed reordering of object arrays

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'babrahams:editable-list',
   summary: 'Editable list widget for Meteor',
-  version: '0.6.3',
+  version: '0.6.4',
   git: 'https://github.com/jackadams/meteor-editable-list.git'
 });
 
