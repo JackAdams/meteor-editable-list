48c8897bc4d0fc820b086ddd9ccb1027706c04ef
Brent Abrahams <brent_abrahams@yahoo.com>
2021-07-22 00:16:03 +0800

    Updated for 2.3

--- package.js
+++ package.js
@@ -1,23 +1,21 @@
 Package.describe({
   name: 'babrahams:editable-list',
   summary: 'Editable list widget for Meteor',
-  version: '0.6.4',
+  version: '0.6.10',
   git: 'https://github.com/jackadams/meteor-editable-list.git'
 });
 
-Package.onUse(function(api) {
+Package.onUse(function (api) {
     
-  api.versionsFrom('1.0');
+  api.versionsFrom(['1.8.2', '2.3']);
   
-  api.use('babrahams:editable-text@0.9.12', ['client','server']);
+  api.use('babrahams:editable-text@0.9.17', ['client','server']);
   api.imply('babrahams:editable-text');
-  api.use('jquery', 'client');
+  api.use('jquery@1.11.11', 'client');
   // api.use('mizzao:jquery-ui@1.11.2', 'client'); // Removed in 0.3.0 to give devs flexibility about which version of jqueryui to use, or to leave it out completely
   api.use('tracker', 'client');
   api.use('minimongo', 'client');
-  api.use('templating', 'client');
-  api.use('blaze', 'client');
-  api.use('spacebars', 'client');
+	api.use(['templating@1.3.2', 'blaze@2.3.4', 'spacebars@1.0.15'], 'client');
   api.use('dburles:mongo-collection-instances@0.3.5', ['client','server']);
   api.use('underscore', ['client','server']);
   api.use('mongo', ['client','server']);
@@ -34,7 +32,7 @@ Package.onUse(function(api) {
 
 });
 
-Package.onTest(function(api) {
+Package.onTest(function (api) {
     
   api.use('tinytest');
   api.use('babrahams:editable-list');
