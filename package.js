1017afae3ebb06663b0061da4b5968a378cd0119
Brent Abrahams <brent_abrahams@yahoo.com>
2022-07-30 12:22:31 +0800

    Allow for functions to determine tags' class

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'babrahams:editable-list',
   summary: 'Editable list widget for Meteor',
-  version: '0.6.10',
+  version: '0.6.11',
   git: 'https://github.com/jackadams/meteor-editable-list.git'
 });
 
